384ff6924485f954aad6a491bb5bb730d89242b5
Rohith M <rohithmerala@gmail.com>
2022-02-01 09:52:03 +0530

    Made The Inputs Empty After Entering the number

--- V1.0/v1.js
+++ V1.0/v1.js
@@ -1,18 +1,26 @@
 function add() {
   var result = Number(document.getElementById("num1").value) + Number(document.getElementById("num2").value);
   document.getElementById("ans_label").innerHTML = "The Sum is " + result;
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
 }
 
 function sub() {
   var result = Number(document.getElementById("num1").value) - Number(document.getElementById("num2").value);
   document.getElementById("ans_label").innerHTML = "The Difference is " + result;
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
 }
 
 function mul() {
   var result = Number(document.getElementById("num1").value) * Number(document.getElementById("num2").value);
   document.getElementById("ans_label").innerHTML = "The Product is " + result;
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
 }
 function divi() {
   var result = Number(document.getElementById("num1").value) / Number(document.getElementById("num2").value);
   document.getElementById("ans_label").innerHTML = "The Ans is " + result;
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
 }
